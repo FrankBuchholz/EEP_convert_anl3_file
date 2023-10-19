e6f74b2fe9ce4faedab01bcf97a4a835e5c39693
FrankBuchholz <FrankBuchholz@users.noreply.github.com>
2023-10-19 14:15:42 +0000

    deploy: 59ddc9ccd1169625476f1724ee1be60030050542

--- js/matrix.js
+++ js/matrix.js
@@ -81,4 +81,21 @@ matrix.multiply = function (A, B) {	// Matrix multiplication
 	return product;
 }
 
+matrix.det3x3 = function (A) { // Determinant of the upper left 3x3 sub matrix
+	const [ a11, a12, a13, a14, a21, a22, a23, a24, a31, a32, a33, a34, a41, a42, a43, a44 ] = A;
+	const det = 
+			      (a11 * a22 * a33) + (a21 * a32 * a13) + (a31 * a12 * a23) - (a13 * a22 * a31) - (a23 * a32 * a11) - (a33 * a12 * a21);
+	return det;
+}
+
+matrix.det = function (A) { // Determinant of the 4x4 matrix
+	const [ a11, a12, a13, a14, a21, a22, a23, a24, a31, a32, a33, a34, a41, a42, a43, a44 ] = A;
+	const det = 
+		- a41 * ( (a12 * a23 * a34) + (a22 * a33 * a14) + (a32 * a13 * a24) - (a14 * a23 * a32) - (a24 * a33 * a12) - (a34 * a13 * a22) )
+		+ a42 * ( (a11 * a23 * a34) + (a21 * a33 * a14) + (a31 * a13 * a24) - (a14 * a23 * a31) - (a24 * a33 * a11) - (a34 * a13 * a21) )
+		- a43 * ( (a11 * a22 * a34) + (a21 * a32 * a14) + (a31 * a12 * a24) - (a14 * a22 * a31) - (a24 * a32 * a11) - (a34 * a12 * a21) )
+		+ a44 * ( (a11 * a22 * a33) + (a21 * a32 * a13) + (a31 * a12 * a23) - (a13 * a22 * a31) - (a23 * a32 * a11) - (a33 * a12 * a21) );
+	return det;
+}
+
 })(this); // end module
